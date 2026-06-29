3aa4ede1757a46b795e844727a41afa7f0863a05
maxstepanyuk <40894200+maxstepanyuk@users.noreply.github.com>
2026-06-29 22:55:20 +0000

    Deploying to gh-pages from @ maxstepanyuk/goit-js-hw-11@220594ec0be7671a5f0fe83980d0aa60eec136b2 🚀

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-import{i}from"./assets/vendor-I1I71QQ2.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const f=document.querySelector(".form");f.addEventListener("submit",function(s){s.preventDefault();const t=s.target.elements["search-text"].value.trim();if(t.length<=0){a("empty search","error");return}a(t,"info")});function a(s,o="success"){const t={message:s,position:"bottomRight",timeoot:5e3};switch(o){case"success":i.success(t);break;case"error":i.error(t);break;case"warning":i.warning(t);break;case"info":i.info(t);break;default:i.error({...t,message:"invalid type of tosat"});break}}
+import{a as u,S as p,i as l}from"./assets/vendor-BGqwtSVv.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();const f="https://pixabay.com/api/",g=void 0;function y(a){const s={q:a,image_type:"photo",orientation:"horizontal",safesearch:"true",key:g};return u.get(f,{params:s}).then(({data:e})=>e)}const c=document.querySelector(".loader"),m=document.querySelector(".gallery"),d={captionsData:"alt",captionDelay:250,className:"lightbox-custom"},h=new p(".open-modal-link",d);function b(a){const s=a.map(e=>`
+      <li class="gallery-item">
+        <a
+          class="gallery-link open-modal-link"
+          href="${e.largeImageURL}"
+        >
+          <img
+            class="gallery-image"
+            src="${e.webformatURL}"
+            alt="Photo of ${e.name} by ${e.user}"
+            with="360"
+            height="152"
+          />
+          <ul class="gallery-item-stats">
+            <li class="gallery-item-stat">
+              <p class="gallery-item-stat-name">Likes</p>
+              <p class="gallery-item-stat-value">${e.likes}</p>
+            </li>
+            <li class="gallery-item-stat">
+              <p class="gallery-item-stat-name">Views</p>
+              <p class="gallery-item-stat-value">${e.views}</p>
+            </li>
+            <li class="gallery-item-stat">
+              <p class="gallery-item-stat-name">Comments</p>
+              <p class="gallery-item-stat-value">${e.comments}</p>
+            </li>
+            <li class="gallery-item-stat">
+              <p class="gallery-item-stat-name">Downloads</p>
+              <p class="gallery-item-stat-value">${e.downloads}</p>
+            </li>
+          </ul>
+        </a>
+      </li>`).join("");m.insertAdjacentHTML("beforeend",s),h.refresh()}function L(){m.innerHTML=""}function w(){c.classList.remove("hidden")}function S(){c.classList.add("hidden")}const v=document.querySelector(".form");v.addEventListener("submit",P);function P(a){a.preventDefault();const e=a.target.elements["search-text"].value.trim();if(e.length<=0){n("empty search","error");return}L(),w(),y(e).then(o=>{const{hits:t}=o;t.length<=0?n("Sorry, there are no images matching your search query. Please try again!","warning"):b(t)}).catch(o=>{console.log("Error on getImagesByQuery",o),n("Sorry, there was an error getting images. Please try again!","error")}).finally(()=>{S()})}function n(a,s="success"){const e={message:a,position:"topRight",timeoot:5e3};switch(s){case"success":l.success(e);break;case"error":l.error(e);break;case"warning":l.warning(e);break;case"info":l.info(e);break;default:l.error({...e,message:"invalid type of tosat"});break}}
 //# sourceMappingURL=index.js.map
